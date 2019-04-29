0b0f0fa31d409953254f972972f2feea414c515a
Mikhail Boronin <boroninm@gmail.com>
2019-04-29 09:12:14 +0200

    Update write_dao.py

--- dao/write_dao.py
+++ dao/write_dao.py
@@ -1,8 +1,12 @@
 import pymysql
 
+def save_product_id_match(inset_list):
+    query = """
+        INSERT IGNORE INTO `{database}`.`{table}`
+        (product_id, matchstring, category_id, brand_id)
+        VALUES (%s, %s, %s, %s);
+    """.format(database=config.DB_SCHEMA, table=config.DB_TABLE)
 
-connection = pymysql.connect(host='localhost',
-                                     database='housing',
-                                     user='bot',
-                                     password='1')
-query = """INSERT INTO """
+    conn = base.get_connection()
+    cursor = conn.cursor()
+    cursor.executemany(query, inset_list)
